c08a9c1de1f1987f50c2f0ed472b3f6f042f8b2c
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2024-03-29 01:30:57 +0100

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,31 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_1/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Create Post Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('SuperSecretTitle'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Tap the '+' icon
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Wait for the dialog to appear
+    await tester.pumpAndSettle();
+
+    // Verify that our dialog has appeared
+    expect(find.text('New Post'), findsOneWidget);
+
+    // Enter the title
+    await tester.enterText(find.bySemanticsLabel('Title'), 'SuperSecretTitle');
+
+    // Post
+    await tester.tap(find.text('Post'));
+
+    // Wait for the dialog to disappear
+    await tester.pumpAndSettle();
+
+    // Verify that our post has been created
+    expect(find.text('SuperSecretTitle'), findsOneWidget);
   });
 }
